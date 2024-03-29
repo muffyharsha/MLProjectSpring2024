a5e0b3755f14a015af636f4dfa3df191433eb4da
Sri harsha Patallapalli <patallapalli.s@northeastern.edu>
2024-03-29 02:11:00 -0400

    adding batches

---
+++ crossValidationResnetFullTrain.sh
@@ -0,0 +1,90 @@
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001.log" > ../out/resnet_256_001.out
+
+
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0.1 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001_1.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001_1.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001_0.log" > ../out/resnet_256_001_1.out
+
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0.05 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001_05.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001_05.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001_05.log" > ../out/resnet_256_001_05.out
+
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0.01 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001_01.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001_01.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001_01.log" > ../out/resnet_256_001_01.out
+
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0.005 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001_005.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001_005.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001_005.log" > ../out/resnet_256_001_005.out
+
+
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0.001 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001_001.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001_001.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001_001.log" > ../out/resnet_256_001_001.out
+
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0.0005 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001_0005.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001_0005.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001_0005.log" > ../out/resnet_256_001_0005.out
+
+
+python3.8 fullTrainResnetStrikesBack.py \
+--batchsize 256 \
+--learningrate 0.001 \
+--epochs 60 \
+--seedvalue 60 \
+--weightdecay 0.0001 \
+--filepathstats "C:/Users/sriha/NEU/ML Project/stats/resnet_256_001_0001.csv" \
+--filepathmodel "C:/Users/sriha/NEU/ML Project/models/resnet_256_001_0001.pth" \
+--datasetpath "C:/Users/sriha/NEU/clozy cloth types/garmentStructuredData" \
+--logfilepath "C:/Users/sriha/NEU/ML Project/logs/resnet_256_001_0001.log" > ../out/resnet_256_001_0001.out
